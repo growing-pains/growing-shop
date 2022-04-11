0adcf871f0d96ffcab7a3230d1129ae284ebd9d9
Sung-jin <osj4872@gmail.com>
2022-04-11 19:04:25 +0900

    #3 project init

---
+++ growing-shop-java/src/test/java/com/example/growingshop/GrowingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.growingshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
