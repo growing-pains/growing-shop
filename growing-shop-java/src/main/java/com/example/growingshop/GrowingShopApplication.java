2af616931ac605856b4b06e5ef70e8240cbea086
Sung-jin <osj4872@gmail.com>
2022-04-20 12:26:39 +0900

    Feature/#3 프로젝트 init (#4)

---
+++ growing-shop-java/src/main/java/com/example/growingshop/GrowingShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.growingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingShopApplication.class, args);
+	}
+
+}
