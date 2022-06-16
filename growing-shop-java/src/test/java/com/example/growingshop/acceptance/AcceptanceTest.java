8b92d991ba7f4f7a40dcc619dcde8dfbabf5a164
Sung-jin <osj4872@gmail.com>
2022-06-16 18:46:28 +0900

    fix: 인수 테스트 격리를 위한 기능 누락 수정 및 db 예약어가 테이블명인 경우 double quote 로 감싸도록 수정

--- growing-shop-java/src/test/java/com/example/growingshop/acceptance/AcceptanceTest.java
+++ growing-shop-java/src/test/java/com/example/growingshop/acceptance/AcceptanceTest.java
@@ -1,11 +1,13 @@
 package com.example.growingshop.acceptance;
 
+import com.example.growingshop.acceptance.helper.DatabaseCleanup;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.Filter;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -28,6 +30,9 @@ public class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
     public static RequestSpecification defaultSpec;
     public static RequestSpecification failResponseSpec;
 
@@ -36,6 +41,8 @@ public class AcceptanceTest {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+
+        databaseCleanup.execute();
 
         defaultSpec = new RequestSpecBuilder()
                 .addFilter(defaultAssuredFilter(restDocumentation))
