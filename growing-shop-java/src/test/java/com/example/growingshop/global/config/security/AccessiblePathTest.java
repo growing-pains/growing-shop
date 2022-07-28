692a30ae3b4e68e784c3cf2f55002279bc427829
Sung-jin <osj4872@gmail.com>
2022-07-28 13:10:35 +0900

    test: path 별 권한 설정 filter 테스트 추가

--- growing-shop-java/src/test/java/com/example/growingshop/global/config/security/AccessiblePathTest.java
+++ growing-shop-java/src/test/java/com/example/growingshop/global/config/security/AccessiblePathTest.java
@@ -11,57 +11,58 @@ import com.example.growingshop.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AccessiblePathTest {
 
-    @Mock
-    private UserRepository userRepository;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private PrivilegeService privilegeService = mock(PrivilegeService.class);
 
-    @Mock
-    private PrivilegeService privilegeService;
+    private HttpServletRequest request = spy(HttpServletRequest.class);
+    private Authentication authentication = spy(Authentication.class);
 
     @InjectMocks
     private AccessiblePath accessiblePath;
 
-    @Spy
-    private HttpServletRequest request;
+    private static final String adminAllowPath = "/admin";
+    private static final String sellerAllowPath = "/seller";
+    private static final String normalAllowPath = "/normal";
 
-    @Spy
-    private Authentication authentication;
+    private static final Privilege adminPrivilege = new Privilege("admin", adminAllowPath, "");
+    private static final Privilege sellerPrivilege = new Privilege("seller", sellerAllowPath, "");
+    private static final Privilege normalPrivilege = new Privilege("normal", normalAllowPath, "");
 
-    private Privilege adminPrivilege = new Privilege("admin", "/admin", "");
-    private Privilege sellerPrivilege = new Privilege("seller", "/seller", "");
-    private Privilege normalPrivilege = new Privilege("normal", "/normal", "");
-
-    private Roles adminRole = new Roles(Collections.singletonList(
+    private static final Roles adminRole = new Roles(Collections.singletonList(
             new Role("ADMIN", Arrays.asList(adminPrivilege, sellerPrivilege, normalPrivilege))
     ));
-    private Roles sellerRole = new Roles(Collections.singletonList(
+    private static final Roles sellerRole = new Roles(Collections.singletonList(
             new Role("SELLER", Arrays.asList(sellerPrivilege, normalPrivilege))
     ));
-    private Roles normalRole = new Roles(Collections.singletonList(
+    private static final Roles normalRole = new Roles(Collections.singletonList(
             new Role("NORMAL", Collections.singletonList(normalPrivilege))
     ));
 
-    private User admin = User.builder().type(UserType.ADMIN).roles(adminRole).build();
-    private User seller = User.builder().type(UserType.NORMAL).roles(sellerRole).build();
-    private User normal = User.builder().type(UserType.SELLER).roles(normalRole).build();
+    private static final User admin = User.builder().type(UserType.ADMIN).roles(adminRole).build();
+    private static final User seller = User.builder().type(UserType.NORMAL).roles(sellerRole).build();
+    private static final User normal = User.builder().type(UserType.SELLER).roles(normalRole).build();
 
     @BeforeEach
     void beforeEach()
@@ -84,17 +85,47 @@ class AccessiblePathTest {
         assertThat(result).isTrue();
     }
 
-    @Test
-    void path_에_대한_권한이_없는_사용자가_요청하면_접근이_불가능해야_한다() {
+    @ParameterizedTest
+    @MethodSource("notAllowPathParameterizedTestData")
+    void path_에_대한_권한이_없는_사용자가_요청하면_접근이_불가능해야_한다(User user) {
         // given
-        when(request.getPathInfo()).thenReturn("/admin");
-        when(authentication.getPrincipal()).thenReturn("");
-        when(userRepository.findUsersByLoginId(any())).thenReturn(Optional.of(seller));
+        when(request.getPathInfo()).thenReturn(adminAllowPath);
+        when(userRepository.findUsersByLoginId(any())).thenReturn(Optional.of(user));
 
         // when
-        boolean result = accessiblePath.check(request, authentication);
+        boolean resultBySeller = accessiblePath.check(request, authentication);
 
         // then
-        assertThat(result).isFalse();
+        assertThat(resultBySeller).isFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource("allowPathParameterizedTestData")
+    void path_에_권한이_있는_사용자가_요청하면_접근이_가능해야_한다(User user, List<String> paths) {
+        when(userRepository.findUsersByLoginId(any())).thenReturn(Optional.of(user));
+        paths.forEach(path -> {
+            when(request.getPathInfo()).thenReturn(path);
+
+            // when
+            boolean result = accessiblePath.check(request, authentication);
+
+            // then
+            assertThat(result).isTrue();
+        });
+    }
+
+    private static Stream<Arguments> notAllowPathParameterizedTestData() {
+        return Stream.of(
+                Arguments.of(seller),
+                Arguments.of(normal)
+        );
+    }
+
+    private static Stream<Arguments> allowPathParameterizedTestData() {
+        return Stream.of(
+                Arguments.of(admin, Arrays.asList(adminAllowPath, sellerAllowPath, normalAllowPath)),
+                Arguments.of(seller, Arrays.asList(sellerAllowPath, normalAllowPath)),
+                Arguments.of(normal, Collections.singletonList(normalAllowPath))
+        );
     }
 }
