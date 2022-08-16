12d513f7bba0ce7f51a15728bb53c4145ddf96b2
Sung-jin <osj4872@gmail.com>
2022-08-16 18:15:51 +0900

    refactor: 기존 서비스의 반환값을 dto 로 변경

--- growing-shop-java/src/main/java/com/example/growingshop/domain/auth/dto/AuthRequest.java
+++ growing-shop-java/src/main/java/com/example/growingshop/domain/auth/dto/AuthRequest.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthRequest {
 
     @Getter
