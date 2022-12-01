cc0c714428b264e29a2a065cb970714c69a572d1
Sung-jin <osj4872@gmail.com>
2022-12-01 19:13:45 +0900

    멀티 모듈로 분리
    - 오디언스
    - 인증 (유저/업체)
    - 주문
    - 상품

---
+++ growing-shop-product/src/test/java/com/example/growingshopproduct/GrowingShopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.growingshopproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingShopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
