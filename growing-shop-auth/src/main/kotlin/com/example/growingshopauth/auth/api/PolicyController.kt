ac10776f6e938b6037fc0f6047c59b906e023729
Sung-jin <osj4872@gmail.com>
2022-12-13 13:02:07 +0900

    인증 모듈의 인증 api 컨버팅

---
+++ growing-shop-auth/src/main/kotlin/com/example/growingshopauth/auth/api/PolicyController.kt
@@ -0,0 +1,28 @@
+package com.example.growingshopauth.auth.api
+
+import com.example.growingshopauth.auth.dto.RoleRequest
+import com.example.growingshopauth.auth.dto.RoleResponse
+import com.example.growingshopauth.auth.service.PolicyService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/policies")
+class PolicyController(
+    private val policyService: PolicyService
+) {
+
+    @PostMapping
+    fun create(@RequestBody req: RoleRequest.CreatePolicy): ResponseEntity<RoleResponse.PoliciesRes> = ResponseEntity(
+        RoleResponse.PoliciesRes.from(policyService.create(req)),
+        HttpStatus.OK
+    )
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Any> {
+        policyService.delete(id)
+
+        return ResponseEntity(HttpStatus.OK)
+    }
+}
