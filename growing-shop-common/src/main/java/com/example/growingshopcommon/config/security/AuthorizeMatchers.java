fe622d337c506e87130b8f7f33339803d203cf34
Sung-jin <osj4872@gmail.com>
2023-01-01 15:59:18 +0900

    모듈별 설정 추가될 수 있도록 yml 파일 이름 변경

--- growing-shop-common/src/main/java/com/example/growingshopcommon/config/security/AuthorizeMatchers.java
+++
@@ -1,9 +0,0 @@
-package com.example.growingshopcommon.config.security;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry;
-
-public interface AuthorizeMatchers {
-    AuthorizationManagerRequestMatcherRegistry configMatchers(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry);
-}
